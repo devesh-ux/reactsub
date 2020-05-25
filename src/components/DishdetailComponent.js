f8b77df2ff4d7a1e713abf0d1ebbf21a3babf3c5
DEVESH KUMAR TRIVEDI <trivedidevesh6076@gmail.com>
2020-05-25 14:18:27 +0530

    presentational and container component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
+import {Card,CardImg,CardText,CardBody,CardTitle} from 'reactstrap';
 
 
 class DishDetail extends Component {
@@ -7,22 +7,71 @@ class DishDetail extends Component {
         super(props);
     }
 
+    renderDish(selectedDish){
+        
+        if(selectedDish!=null){
+            return(
+                    <Card>
+                        <CardImg width="100%" src ={selectedDish.image} alt={selectedDish.name}/>
+                        <CardBody>
+                            <CardTitle>{selectedDish.name}</CardTitle>
+                            <CardText>{selectedDish.description}</CardText>
+                        </CardBody>
+                    </Card>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+    renderComments(selectedDish) {
+        console.log("devesh");
+        if (selectedDish == null) {
+           return (<div></div>)
+       }
+   const cmnts = selectedDish.map(comments => {
+       return (
+           <li key={comments.id}>
+               <p>{comments.comment}</p>
+               <p>-- {comments.author},
+               &nbsp;
+               {new Intl.DateTimeFormat('en-US', {
+                       year: 'numeric',
+                       month: 'long',
+                       day: '2-digit'
+                   }).format(new Date(comments.date))}
+               </p>
+           </li>
+       )
+   })
+   return (
+       <div>
+           <h4> Comments </h4>
+           <ul className='list-unstyled'>
+               {cmnts}
+           </ul>
+
+       </div>
+   )
+}
     render(){
 
-
-        const details = {
-            return(
-                <div className="col-12 col-md-5 mt-1">
-                
-                </div>
-            )
-        }
+       
         return(
+           
             <div className="row">
-                {details}
+                <div className="col-12 col-md-5 mt-1">
+                    {this.renderDish(this.props.selectedDish)}
+                </div>
+                <div className="col-12 col-md-5 mt-1">
+                    {this.renderComments(this.props.selectedDish)}
+                </div>
             </div>
+
         );
     }
 }
 
-export default DishDetail ;
+export default DishDetail ; 
